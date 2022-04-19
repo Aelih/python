2b82bad83a6aa0b95c7386c6333ad263c0c163ea
Aelih <jamis460773@gmail.com>
2022-04-19 20:53:53 +0700

    Рабочий вариант с корректным парсингом ссылок

--- JoyLeakParse.py
+++ JoyLeakParse.py
@@ -3,31 +3,29 @@ from cgitb import html
 from operator import truediv
 import requests
 from bs4 import BeautifulSoup
-#import validators
+import validators
 from time import sleep
 import pandas as pd
 import os
-import re
 
 # %%
 stdurl = "http://joyreactor.cc"
 #starturl = "http://joyreactor.cc/tag/%D0%AD%D1%80%D0%BE%D1%82%D0%B8%D0%BA%D0%B0"
-starturl = "http://joyreactor.cc/tag/%D0%AD%D1%80%D0%BE%D1%82%D0%B8%D0%BA%D0%B0/4511"
+starturl = "http://joyreactor.cc/tag/%D0%AD%D1%80%D0%BE%D1%82%D0%B8%D0%BA%D0%B0/4509"
 dataleaklinks = []
-sleeptime = 1
+sleeptime = 0
 PagesRange = 2
-desktoppath = os.path.join((os.environ['USERPROFILE']), 'Desktop') 
+desktoppath = os.path.join((os.environ['USERPROFILE']), 'Desktop')
 
 def CorrectUrl(commenttext):
-    #if validators.url(commenttext) == True and commenttext.find('instagram') == 0 and commenttext.find('joyreactor') == 0:
-    if commenttext.find('://') > 0 and commenttext.find('instagram') == 0 and commenttext.find('joyreactor') == 0: 
+    if validators.url(commenttext) == True and commenttext.find('instagram') == -1 and commenttext.find('joyreactor') == -1: 
         return True
     else:
         return False
 
 # %%
 Startpage = requests.get(starturl)
-#sleep(sleeptime)
+sleep(sleeptime)
 SoupStartpage = BeautifulSoup(Startpage.text, "html.parser")
 
 for i in range(PagesRange):
@@ -41,17 +39,20 @@ for i in range(PagesRange):
 
     for datapostlink in datapostlinks:
         respost = requests.get(datapostlink)
-        #sleep(sleeptime)
+        sleep(sleeptime)
         soup = BeautifulSoup(respost.text, "html.parser")
         comments = soup.findAll('div', class_='comment')
 
         for comment in comments:
-            commenttext = comment.find('a').text
-            if CorrectUrl(commenttext) == True:
-                dataleaklinks.append([commenttext])
+            soupcomment = BeautifulSoup(str(comment), "html.parser")
+            refs = soupcomment.findAll('a')
+            
+            for ref in refs:
+                if CorrectUrl(ref.text) == True:
+                    dataleaklinks.append([ref.text])
     
     Startpage = requests.get(NextPageUrl)
-    #sleep(sleeptime)
+    sleep(sleeptime)
     SoupStartpage = BeautifulSoup(Startpage.text, "html.parser")
 
 
