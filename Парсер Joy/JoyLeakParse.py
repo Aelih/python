e1b4edb7f0c1b16c95531ec3bea62f5bca0cce44
Aelih <jamis460773@gmail.com>
2022-04-21 22:01:38 +0700

    Удален неиспользуемый код

--- Парсер Joy/JoyLeakParse.py
+++ Парсер Joy/JoyLeakParse.py
@@ -121,19 +121,7 @@ class MainForm(Frame):
         self.style = Style()
         self.style.theme_use("default")
         self.pack(fill=BOTH, expand=1)
-        #self.centerWindow()
         addElements(self)
-
-    def centerWindow(self):
-        w = 300
-        h = 150
-
-        sw = self.parent.winfo_screenwidth()
-        sh = self.parent.winfo_screenheight()
-
-        x = (sw - w) / 2
-        y = (sh - h) / 2
-        self.parent.geometry('%dx%d+%d+%d' % (w, h, x, y))
 
 # Создаём главное-корневое окно
 def main():
