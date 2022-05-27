1b317c0d81b49511ac23d927df9b7437844df69c
AlexProvotorov <jamis460773@gmail.com>
2022-05-27 17:21:20 +0700

    Доработки по асинхронности

--- JoyParse/qt_async_JoyLeakParse.py
+++ JoyParse/qt_async_JoyLeakParse.py
@@ -20,7 +20,7 @@ desktoppath = path.join((environ['USERPROFILE']), 'Desktop')
 chromepath = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s --incognito"
 
  # Проверка на корректность URL
-def CorrectUrl(self, commenttext):
+async def CorrectUrl(commenttext):
     if url(commenttext) == True and commenttext.find('instagram') == -1 and commenttext.find('reactor') == -1:
         return True
     else:
@@ -34,6 +34,55 @@ async def ReadPageSoup(pageUrl):
             SoupStartpage = BeautifulSoup(StartpageText, "html.parser")
             return SoupStartpage
 
+async def get_URLs(pageUrl, PagesRange):
+    urlList = []
+    urlList.append[pageUrl]
+
+    async with aiohttp.ClientSession() as session:
+        for i in range(PagesRange):
+            async with session.get(pageUrl) as response:
+                StartpageText = await response.text()
+                SoupStartpage = BeautifulSoup(StartpageText, "html.parser")
+                pageUrl = stdurl+SoupStartpage.find('a', class_='next').get('href')
+                urlList.append[pageUrl]
+
+    return urlList
+
+async def get_links(url):
+    leakurllist = []
+
+    ParseTasks = []
+    ParseTasks.append(asyncio.create_task(ReadPageSoup(url)))
+
+    Result = await asyncio.gather(*ParseTasks)
+    SoupStartpage = Result[0]
+
+    posts = SoupStartpage.findAll('span', class_='link_wr')
+    datapostlinks = []
+
+    for post in posts:
+        postlink = stdurl+post.find('a', class_='link').get('href')
+        datapostlinks.append(postlink)
+
+    for datapostlink in datapostlinks:
+        datapostlinkTasks = []
+        datapostlinkTasks.append(asyncio.create_task(ReadPageSoup(datapostlink)))
+
+        datapostlinkResult = await asyncio.gather(*datapostlinkTasks)
+        soup = datapostlinkResult[0]
+
+        comments = soup.findAll('div', class_='comment')
+
+        for comment in comments:
+            soupcomment = BeautifulSoup(str(comment), "html.parser")
+            refs = soupcomment.findAll('a')
+
+            for ref in refs:
+                if CorrectUrl(ref.text) == True:
+                    leakurllist.append(ref.text)
+
+    return leakurllist     
+
 # Разбор комментариев
 async def ParseComments(self):
     # Обновляем переменные забирая значения из окна
@@ -44,38 +93,23 @@ async def ParseComments(self):
     #self.ProgressBarInit(PagesRange)
 
     # Читает начальную страницу
+    urlTasks = []
+    urlTasks.append(asyncio.create_task(get_URLs(starturl, PagesRange)))
+
+    urlList = await asyncio.gather(*urlTasks) 
+
     tasks = []
-    tasks.append(asyncio.create_task(ReadPageSoup(starturl)))
+    for urlElem in urlList:
+        for url in urlElem:
+            tasks.append(asyncio.create_task(get_links(url)))  
  
-    SoupStartpage = self.ReadPageSoup(starturl)
+    results = await asyncio.gather(*tasks)
 
-    for i in range(PagesRange):
-        NextPageUrl = stdurl+SoupStartpage.find('a', class_='next').get('href')
-        posts = SoupStartpage.findAll('span', class_='link_wr')
-        datapostlinks = []
-
-        for post in posts:
-            postlink = stdurl+post.find('a', class_='link').get('href')
-            datapostlinks.append(postlink)
-
-        for datapostlink in datapostlinks:
-            respost = requests.get(datapostlink)
-            sleep(sleeptime)
-            soup = BeautifulSoup(respost.text, "html.parser")
-            comments = soup.findAll('div', class_='comment')
-
-            for comment in comments:
-                soupcomment = BeautifulSoup(str(comment), "html.parser")
-                refs = soupcomment.findAll('a')
-
-                for ref in refs:
-                    if self.CorrectUrl(ref.text) == True:
-                        self.RequestLoop.emit(ref.text, datapostlink)
-            
-        self.BarIterator.emit(i)
-
-        # Читает следующую страницу (кнопка Вперед)
-        SoupStartpage = self.ReadPageSoup(NextPageUrl)
+    for result in results:
+        for leakurl in result:
+            item = QtWidgets.QListWidgetItem()
+            item.setText(leakurl)
+            self.ui.listWidget.addItem(item)
     
     # self.SaveToCsv()
 
@@ -96,28 +130,28 @@ class MainWindow(QtWidgets.QMainWindow):
     def Run(self):
         asyncio.run(ParseComments(self)) 
 
-    # Метод-слот, срабатывающий при сигнале с другого потока
-    @QtCore.pyqtSlot(str, str)
-    def ResultFixation(self, urltext, urlpost):
-        dataleaklinks.append([urltext, urlpost])
-        self.AddListElement(urltext)
+    # # Метод-слот, срабатывающий при сигнале с другого потока
+    # @QtCore.pyqtSlot(str, str)
+    # def ResultFixation(self, urltext, urlpost):
+    #     dataleaklinks.append([urltext, urlpost])
+    #     self.AddListElement(urltext)
 
-    @QtCore.pyqtSlot(int)
-    def ProgressBarIterator(self, i):
-        self.ui.progressBar.setValue(i+1)
+    # @QtCore.pyqtSlot(int)
+    # def ProgressBarIterator(self, i):
+    #     self.ui.progressBar.setValue(i+1)
 
-    # Добавление элемента в список
-    def AddListElement(self, urltext):
-        item = QtWidgets.QListWidgetItem()
-        item.setText(urltext)
-        self.ui.listWidget.addItem(item)
+    # # Добавление элемента в список
+    # def AddListElement(self, urltext):
+    #     item = QtWidgets.QListWidgetItem()
+    #     item.setText(urltext)
+    #     self.ui.listWidget.addItem(item)
 
-    # Инициализация progress bar
-    def ProgressBarInit(self, PagesRange):
-        self.ui.progressBar.setValue(0)
-        # self.ui.progressBar.setMinimum = 0
-        # self.ui.progressBar.setMaximum = PagesRange
-        self.ui.progressBar.setRange(0, PagesRange)
+    # # Инициализация progress bar
+    # def ProgressBarInit(self, PagesRange):
+    #     self.ui.progressBar.setValue(0)
+    #     # self.ui.progressBar.setMinimum = 0
+    #     # self.ui.progressBar.setMaximum = PagesRange
+    #     self.ui.progressBar.setRange(0, PagesRange)
 
     # Сохраняем таблицу в CSV через pandas
     def SaveToCsv(self):
@@ -125,8 +159,6 @@ class MainWindow(QtWidgets.QMainWindow):
         df = pd.DataFrame(dataleaklinks, columns=header)
         df.to_csv(desktoppath+'\leaked.csv', sep=';', encoding='utf8')
         QtWidgets.QMessageBox.information(self, "Готово!", "Файл находится тут: "+ desktoppath+"\leaked.csv")
-
-   
 
     def OpenClickedItem(self, item):
         webbrowser.get(chromepath).open(item.text())
