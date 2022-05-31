2add0809a41f06c544160835e0cf81e757d6ed45
Aelih <jamis460773@gmail.com>
2022-05-31 20:57:17 +0700

    Добавлена переменная с заголовками

--- JoyParse/qt_async_JoyLeakParse.py
+++ JoyParse/qt_async_JoyLeakParse.py
@@ -18,6 +18,8 @@ sleeptime = 3
 PagesRange = 20
 desktoppath = path.join((environ['USERPROFILE']), 'Desktop')
 chromepath = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s --incognito"
+headers = {"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.0.0 Safari/537.36"}
 
  # Проверка на корректность URL
 async def CorrectUrl(commenttext):
