0781dd047f33bcce030dda7728ffb5b1041dd457
Aelih <jamis460773@gmail.com>
2022-05-31 22:05:44 +0700

    Добавлен ввод в консоли логина и пароля

--- JoyParse/joy_auth.py
+++ JoyParse/joy_auth.py
@@ -1,19 +1,19 @@
-from email import header
-from urllib import response
 import requests
 from bs4 import BeautifulSoup
 import fake_useragent
 
+session = requests.Session()
+
 link = "https://api.joyreactor.cc/graphql"
 
-query = "mutation Login($login: String!, $password: String!){login(name:$login,password:$password) {me {token}}}"
-
-variables = {"login": "fsdfsd",
-        "password": "sdfsdfw4345"}
+login = input("Enter LOGIN: ")
+passwd = input("Enter PASSWORD: ")
+variables = {"query":"mutation Login($login: String!, $password: String!){login(name:$login,password:$password) {me {token}}}",
+                "variables":{"login":login,"password":passwd}}
 
 user = fake_useragent.UserAgent().random
 
 header = {"user-agent":user}
 
-response = requests.post(link, data=variables, headers=header).text
+response = session.post(link, data=variables, headers=header).text
 print(response)
