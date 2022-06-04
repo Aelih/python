6e47803f3370c8c00832fa3c44244c1f0f37cc8f
Aelih <jamis460773@gmail.com>
2022-06-04 15:47:49 +0700

    Реализован асинхронный вызов авторизации

--- JoyParse/joy_auth.py
+++ JoyParse/joy_auth.py
@@ -1,4 +1,6 @@
+from difflib import restore
 import requests
+import aiohttp
 
 def syncAuthorize(login, passwd):
     session = requests.Session()
@@ -21,5 +23,27 @@ def syncAuthorize(login, passwd):
         else:    
             return [response.json().get('data').get('login').get('me').get('token'), response.status_code]
     else:
-        return [None, response.status_code]      
+        return [None, response.status_code] 
+
+async def asyncAuthorize(login, passwd):
+    link = "https://api.joyreactor.cc/graphql"
+
+    variables = {"query":"mutation Login($login: String!, $password: String!){login(name:$login,password:$password) {me {token}}}",
+                    "variables":{"login":login,"password":passwd}}
+
+    header = {"accept": "*/*",
+                "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36 Edg/101.0.1210.47",
+                "origin":"http://joyreactor.cc",
+                "referer": "http://joyreactor.cc/"}
+ 
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url=link, json=variables, headers=header) as response:
+            result = await response.json(), response.status 
+            if result[1] == 200:
+                if type(result[0].get('data')) == type(None):
+                    return [None, result[1]]        
+                else:    
+                    return [result[0].get('data').get('login').get('me').get('token'), result[1]]
+            else:
+                return [None, result[1]]            
 
--- JoyParse/testAuth.py
+++ JoyParse/testAuth.py
@@ -1,11 +1,22 @@
 import joy_auth as auth
+import asyncio
 
-login = input("Enter LOGIN: ")
-passwd = input("Enter PASSWORD: ")
-result = auth.syncAuthorize(login, passwd)
+async def AuthTry(login, passwd):
+    #result = auth.syncAuthorize(login, passwd)
 
-if result[0] == None:
-    print("Problems with authorization. Please try again!")
-    print(f"Status {result[1]}")
-else:
-    print(result[0]) 
+    Tasks = []
+    Tasks.append(asyncio.create_task(auth.asyncAuthorize(login, passwd)))
+
+    result = await asyncio.gather(*Tasks)
+
+    if result[0][0] == None:
+        print("Problems with authorization. Please try again!")
+        print(f"Status {result[0][1]}")
+    else:
+        print(result[0][0]) 
+
+
+if __name__ == "__main__":
+    login = input("Enter LOGIN: ")
+    passwd = input("Enter PASSWORD: ")
+    asyncio.run(AuthTry(login, passwd))
