1531aba4d345bb41e1879d018cc7190f0c33df47
AlexProvotorov <jamis460773@gmail.com>
2022-06-06 13:19:01 +0700

    Скрипт теста авторизации с возможностью выбора синх или асинх

--- JoyParse/testAuth.py
+++ JoyParse/testAuth.py
@@ -2,8 +2,6 @@ import joy_auth as auth
 import asyncio
 
 async def AuthTry(login, passwd):
-    #result = auth.syncAuthorize(login, passwd)
-
     Tasks = []
     Tasks.append(asyncio.create_task(auth.asyncAuthorize(login, passwd)))
 
@@ -15,8 +13,22 @@ async def AuthTry(login, passwd):
     else:
         print(result[0][0]) 
 
+def syncAuthTry(login, passwd):
+    result = auth.syncAuthorize(login, passwd)
+
+    if result[0] == None:
+        print("Problems with authorization. Please try again!")
+        print(f"Status {result[1]}")
+    else:
+        print(result[0])         
+
 
 if __name__ == "__main__":
+    choice = input("Enter 0 - for Sync, 1 - for Async: ")
     login = input("Enter LOGIN: ")
     passwd = input("Enter PASSWORD: ")
-    asyncio.run(AuthTry(login, passwd))
+
+    if choice == "0":
+        syncAuthTry(login, passwd)
+    else:
+        asyncio.run(AuthTry(login, passwd))
